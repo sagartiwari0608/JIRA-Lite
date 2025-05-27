00f145dfcaec7c53812a08545b9183467e0a591c
sagartiwari0608 <sagartiwarisrt@gmail.com>
2025-05-28 00:22:45 +0530

    did something and pushing it

---
+++ React/src/App.tsx
@@ -0,0 +1,14 @@
+import { type ReactElement } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar';
+
+function App() : ReactElement{
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ React/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar">
+        <ul className="navbar-menu">
+            <li className="navbar-item">Home</li>
+            <li className="navbar-item">About</li>
+            <li className="navbar-item">Contact</li>
+        </ul>
+    </nav>
+    </>
+  )
+}
